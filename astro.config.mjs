ecb90c39e0357af278cf358737900c2d85224f7a
Evelyn <evelyn.ciara.345@gmail.com>
2026-04-11 09:01:48 -0400

    add sitemap-xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
 
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
+
+import { WEBSITE_ROOT_URL } from "./src/constants";
 
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon(), sitemap()],
+  site: WEBSITE_ROOT_URL,
 });
