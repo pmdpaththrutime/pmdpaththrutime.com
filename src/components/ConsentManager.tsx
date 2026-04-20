3db3e2cb1cad67acde64f565599c734fbaef6891
Evelyn <evelyn.ciara.345@gmail.com>
2026-04-20 16:05:10 -0400

    Show banner to all users on first visit

--- src/components/ConsentManager.tsx
+++ src/components/ConsentManager.tsx
@@ -11,6 +11,28 @@ export default function ConsentManager() {
       options={{
         mode: "offline",
         consentCategories: ["necessary", "marketing", "measurement"],
+        offlinePolicy: {
+          policyPacks: [
+            {
+              id: "global-opt-in-banner",
+              match: { isDefault: true },
+              consent: { model: "opt-in", expiryDays: 365 },
+              ui: { mode: "banner" },
+            },
+          ],
+        },
+        i18n: {
+          locale: "en",
+          messages: {
+            en: {
+              cookieBanner: {
+                title: "Come to the dark side\u2026",
+                description:
+                  "This website uses cookies to enable certain features, such as the Spotify embedded player. You'll need to accept marketing cookies for the player to load."
+              }
+            }
+          }
+        }
       }}
     >
       <ConsentBanner />
